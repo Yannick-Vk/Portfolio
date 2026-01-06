a7de29261fe069203805fd0ac998df0770f6d16a
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-06 13:40:53 +0100

    Moved to react based navbar, added base links

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+﻿export interface Props {}
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+export default function Navbar(props: Props) {
+    return (
+        <>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem className="flex flex-row gap-3 justify-center">
+                            <NavigationMenuLink>Home</NavigationMenuLink>
+                            <NavigationMenuLink>About</NavigationMenuLink>
+                            <NavigationMenuLink>Projects</NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </>
+    );
+}
