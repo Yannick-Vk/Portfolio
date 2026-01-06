90cb286473b38d0f0a5a8e24305b39b87269c39a
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-06 14:07:11 +0100

    Changed to map instead of hardcoded links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,21 +9,27 @@ import {
 } from "@/components/ui/navigation-menu"
 
 export default function Navbar(props: Props) {
+    const links = [
+        {link: "/", label: "Home"},
+        {link: "/about", label: "About"},
+        {link: "/projects", label: "Projects"},
+    ];
+
     return (
         <>
             <nav className="flex justify-center w-full py-4 bg-slate-200">
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem className="flex flex-row gap-3 justify-center">
-                            <NavigationMenuLink>
-                                <a href="/">Home</a>
-                            </NavigationMenuLink>
-                            <NavigationMenuLink>
-                                <a href="/about">About</a>
-                            </NavigationMenuLink>
-                            <NavigationMenuLink>
-                                <a href="/projects">Projects</a>
-                            </NavigationMenuLink>
+                            {links.map(({link, label}) => (
+                                <NavigationMenuLink
+                                    key={link}
+                                    href={link}
+                                    className="px-3 py-2 rounded-md text-sm font-medium text-gray-700"
+                                >
+                                    {label}
+                                </NavigationMenuLink>
+                            ))}
                         </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu>
