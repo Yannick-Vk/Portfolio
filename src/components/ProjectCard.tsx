7b43f6f000327d498d455244b250b818987cf3ed
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-07 10:06:29 +0100

    Removed link to GitHub, changed link to details to a button

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import type {Project} from "@/Types.ts";
+import {Button} from "@/components/ui/button.tsx";
 
 export interface Props {
     project: Project;
@@ -11,12 +12,9 @@ export default function ProjectCard(props: Props) {
                 className="border border-gray-300 dark:border-slate-700 rounded-lg bg-accent p-4 shadow-sm hover:shadow-md transition-shadow duration-300">
                 <h2 className="text-2xl font-bold mb-2">{props.project.title}</h2>
                 <p className="">{props.project.description}</p>
-                <a className={"text-indigo-700 dark:text-cyan-200 underline block"} href={props.project.link}
-                   target="_blank">Link
-                    to the github page</a>
-                <a className={"text-indigo-700 dark:text-cyan-200 underline block"}
-                   href={`/projects/${props.project.slug}`} target="_blank">Link
-                    to the details page</a>
+                <Button className={"my-3 bg-fuchsia-400"}>
+                    <a href={`/projects/${props.project.slug}`} target="_blank">Go to details</a>
+                </Button>
                 <p className="text-sm text-gray-500 mt-3">
                     Created on: {props.project.created_at}
                 </p>
