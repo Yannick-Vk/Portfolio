fc60bb7ad5613172ba5eb47deb8dbd21c70e628a
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-07 09:37:33 +0100

    Added dark border to card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,7 +8,7 @@ export default function ProjectCard(props: Props) {
     return (
         <>
             <div
-                className="border border-gray-300 rounded-lg bg-accent p-4 shadow-sm hover:shadow-md transition-shadow duration-300">
+                className="border border-gray-300 dark:border-slate-700 rounded-lg bg-accent p-4 shadow-sm hover:shadow-md transition-shadow duration-300">
                 <h2 className="text-2xl font-bold mb-2">{props.project.title}</h2>
                 <p className="">{props.project.description}</p>
                 <a className={"text-indigo-700 dark:text-cyan-200 underline"} href={props.project.link} target="_blank">Link
