b972c02aa3ff54eb0f83c9d503b833cd46ff164a
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-07 08:37:27 +0100

    Created base for astro content

---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+// 1. Import utilities from `astro:content`
+import {defineCollection} from 'astro:content';
+
+// 2. Import loader(s)
+
+// 3. Import Zod
+
+// 4. Define your collection(s)
+const projects = defineCollection({ /* ... */});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = {projects};
