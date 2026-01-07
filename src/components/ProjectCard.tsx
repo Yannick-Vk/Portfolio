6080ed7a28f0b3d525b8f3aa67a5b7964a8921a7
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-07 09:35:38 +0100

    Moved margin from text to date

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,10 +10,10 @@ export default function ProjectCard(props: Props) {
             <div
                 className="border border-gray-300 rounded-lg bg-accent p-4 shadow-sm hover:shadow-md transition-shadow duration-300">
                 <h2 className="text-2xl font-bold mb-2">{props.project.title}</h2>
-                <p className="mb-4">{props.project.description}</p>
+                <p className="">{props.project.description}</p>
                 <a className={"text-indigo-700 dark:text-cyan-200 underline"} href={props.project.link} target="_blank">Link
                     to the github page</a>
-                <p className="text-sm text-gray-500">
+                <p className="text-sm text-gray-500 mt-3">
                     Created on: {props.project.created_at}
                 </p>
             </div>
