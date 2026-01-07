98656a513867c969c7be1655ab21167a772fbd0b
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-07 08:41:04 +0100

    Extended projects content with zod schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,19 @@
 import {defineCollection} from 'astro:content';
 
 // 2. Import loader(s)
+import {glob} from 'astro/loaders';
 
 // 3. Import Zod
+import {z} from 'astro/zod';
 
 // 4. Define your collection(s)
-const projects = defineCollection({ /* ... */});
+const projects = defineCollection({
+    loader: glob({pattern: "**/*.md", base: "./src/data/projects"}),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+    }),
+});
 
 // 5. Export a single `collections` object to register your collection(s)
 export const collections = {projects};
