07956b2493dd34a206a4eb50b13b2a2e51cce552
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-07 15:37:17 +0100

    Moved to unified contact list

---
+++ src/lib/Contact.ts
@@ -0,0 +1,4 @@
+export const Contact = [
+    {name: "GitHub", url: "https://github.com/Yannick-Vk/", icon: "github"},
+    {name: "LinkedIn", url: "https://www.linkedin.com/in/yannick-van-kerckvoorde-901ab61ba/", icon: "linkedin"},
+];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import {twMerge} from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function CombineClassNames(...classes: (string | undefined)[]) {
+    return classes.filter(Boolean).join(' ');
+}
