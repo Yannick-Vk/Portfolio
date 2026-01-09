b9d85be67f6fca9ce4447ddf99cc23428e8a04e3
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-09 09:57:08 +0100

    Moved to flex instead of grid

--- src/components/SkillCategories.tsx
+++ src/components/SkillCategories.tsx
@@ -27,11 +27,11 @@ export default function SkillCategories(props: Props) {
     return (
         <>
             {props.categories.map(category => (
-                <li className={"mb-5"}>
+                <li className={"mb-5"} key={category.title}>
                     <h4 className="font-bold text-xl mt-1 mb-3">{category.title}</h4>
-                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 mb-3 justify-center">
+                    <ul className="flex flex-row flex-wrap gap-3 mb-3 justify-center">
                         {category.skills.map(skill => (
-                            <li className={"bg-gray-200 rounded-lg border border-gray-200 p-3"}>
+                            <li className={"bg-gray-200 rounded-lg border border-gray-200 p-3"} key={skill.name}>
                                 {skill.name}
                             </li>
                         ))}
