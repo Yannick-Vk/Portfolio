2bb24b65e4eea7e121c11e92545d7c87227088c5
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-09 09:27:08 +0100

    Added some spacing after a title

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -12,7 +12,7 @@ export default function ProfileSection(props: Props) {
     return (
         <>
             <div className={cn("my-5", props.className)} id={props.id}>
-                <h2 className={"text-center font-bold text-3xl"}>{props.title}</h2>
+                <h2 className={"text-center font-bold text-3xl mb-2"}>{props.title}</h2>
                 <div className={"text-center text-lg"}>
                     {props.children}
                 </div>
